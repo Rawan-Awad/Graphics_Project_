6eee45479d276f3a6bdfd82cc9fd9c6a266b18cb
Rawan Awad <161856664+Rawan-Awad@users.noreply.github.com>
2025-07-23 22:18:29 +0300

    ssss

--- Proj/level2.cpp
+++ Proj/level2.cpp
@@ -13,13 +13,12 @@ Model_3DS model_flag;
 Model_3DS model_coin;
 
 void initLevel2() {
-    // Load desert scene, finish line, obstacles
-    model_car.Load("models/car/_Subaru-Loyale.3ds");         // Car model (if different from Level 1)
-    model_sign.Load("models/sign/StopSign.3ds");      // Road sign obstacle
-    model_rock.Load("models/rock/Stone 4.3DS");
-    model_desert.Load("models/desert/uploads_files_4614960_Deasert+sell.3ds");
-    model_flag.Load("models/flag/lp_flag3ds.3DS");
-    model_coin.Load("models/flag/uploads_files_4153932_Cartoon_Coin01_3ds.3ds");
+    model_car.Load("Models/car/_Subaru-Loyale.3ds");         // Car model (if different from Level 1)
+    model_sign.Load("Models/StopSign/StopSign.3ds");      // Road sign obstacle
+    model_rock.Load("Models/rock/Stone 4.3DS");
+    model_desert.Load("Models/desert/uploads_files_4614960_Deasert+sell.3ds");
+    model_flag.Load("Models/flag/lp_flag3ds.3DS");
+    model_coin.Load("Models/coin/uploads_files_4153932_Cartoon_Coin01_3ds.3ds");
 }
 
 void drawLevel2() {
